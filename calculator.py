c2a30703d46181f024a5534401ddd692fb741ea6
yesak1 <23kkazliner@athenian.org>
2020-09-15 20:49:48 +0000

    task 4 done

--- calculator.py
+++ calculator.py
@@ -1,4 +1,40 @@
 '''
 Collaborators: 
+Kasey
 
 '''
+def addition():
+    add=num1+num2
+    return add
+def subtract():
+    sub=num1-num2
+    return sub
+def multiply():
+    mult=num1*num2
+    return mult
+def divide():
+    div=num1%num2
+    return div
+
+
+
+num1=int(input("enter a number"))
+num2=int(input("enter another number"))
+asmd=int(input("enter 1 to add, 2 to subtract, 3 to mulitply, or 4 to divide"))
+
+if asmd==1:
+    print (addition())
+elif asmd==2:
+    print (subtract())
+elif asmd==3:
+    print (multiply())
+elif asmd==4:
+    print (divide())
+    
+    
+
+
+       
+
+
+
